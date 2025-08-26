ba7e1c84d5bf5f7b2426ed2b3dd46aba423c048b
Nicolas Barth <nicolasbarthsouza84@gmail.com>
2025-08-26 08:35:51 -0300

    UPDATE

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: "71y6u3",
+  projectId: "7getux",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
-})
+
+}
+)
